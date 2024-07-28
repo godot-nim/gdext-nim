cdafe181f0560ea093e9e5d41a9d5fedbabb13e8
la .panon. <panno.800k@gmail.com>
2024-07-28 14:05:36 +0900

    rename godot.echo to print and hide generated one

--- src/godot.nim
+++ src/godot.nim
@@ -4,12 +4,15 @@ export InitializationLevel, gdcall
 import godotcore/[ events, builtinindex, geometrics, Variant ]
 export             events, builtinindex, geometrics, Variant
 
-import godotgen/[ utilityfuncs, builtinclasses, classindex, globalenums, localenums, structs ]
-export            utilityfuncs, builtinclasses, classindex, globalenums, localenums, structs
+import godotgen/[ builtinclasses, classindex, globalenums, localenums, structs ]
+export            builtinclasses, classindex, globalenums, localenums, structs
 
 import godot/[ init, classautomate, classtraits, varianttraits, nodetraits, gdrefs, conversions ]
 export         init, classautomate, classtraits, varianttraits, nodetraits, gdrefs, conversions
 
-proc echo*(args: varargs[Variant, variant]) =
+import godotgen/utilityFuncs
+export utilityFuncs except print
+
+proc print*(args: varargs[Variant, variant]) =
   if unlikely(args.len == 0): return
-  print(args[0], args[1..^1])
+  utilityFuncs.print(args[0], args[1..^1])
