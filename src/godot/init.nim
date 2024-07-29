daeffd2272e2f24d1873c868729c24f7d501a9e0
la .panon. <panno.800k@gmail.com>
2024-07-29 12:47:42 +0900

    Load godot APIs before NimMain()

--- src/godot/init.nim
+++ src/godot/init.nim
@@ -1,39 +1,24 @@
 import godotcore/dirty/gdextension_interface
 import godotcore/builtinindex
 import godotcore/events
-import godotcore/eventindex
+import godotcore/staticevents
 import godotcore/commandindex
 import godotcore/extracommands
 import godotcore/typeshift
 import godotgen/utilityfuncs
 
 export events.process
-var initialize_module = event("initialize_module")
 
-var initialize_core* = event("initialize_core")
-var initialize_servers* = event("initialize_servers")
-var initialize_scene* = event("initialize_scene")
-var initialize_editor* = event("initialize_editor")
-var eliminate_core* = event("eliminate_core")
-var eliminate_servers* = event("eliminate_servers")
-var eliminate_scene* = event("eliminate_scene")
-var eliminate_editor* = event("eliminate_editor")
-
-process initialize_module:
-  commandindex.load() # call `getProcAddr` for each interface methods
-  builtinindex.load() # load builtin-classes constructor/destructor for ORC hooks
-  extracommands.load() # load other usual functions to define core library
-  typeshift.load()
-  utilityfuncs.load()
-
-  invoke eventindex.init_engine.on_load_builtinclassConstructor # expects to register by generateds.
-  invoke eventindex.init_engine.on_load_builtinclassOperator
-  invoke eventindex.init_engine.on_load_builtinclassMethod
-  invoke eventindex.init_engine.on_load_metadata
+var initialize_core* = events.event("initialize_core")
+var initialize_servers* = events.event("initialize_servers")
+var initialize_scene* = events.event("initialize_scene")
+var initialize_editor* = events.event("initialize_editor")
+var eliminate_core* = events.event("eliminate_core")
+var eliminate_servers* = events.event("eliminate_servers")
+var eliminate_scene* = events.event("eliminate_scene")
+var eliminate_editor* = events.event("eliminate_editor")
 
 proc initialize(userdata: pointer; p_level: InitializationLevel) {.gdcall.} =
-  invoke initialize_module
-
   case p_level
   # almost all uses is to register user-defined classes
   of Initialization_Core:    invoke initialize_core
@@ -49,26 +34,32 @@ proc deinitialize(userdata: pointer; p_level: InitializationLevel) {.gdcall.} =
   of Initialization_Scene:   invoke eliminate_scene
   of Initialization_Editor:  invoke eliminate_editor
 
-proc init*(p_get_proc_address: InterfaceGetProcAddress; p_library: ClassLibraryPtr; r_initialization: ptr Initialization): Bool {.gdcall.} =
-  try:
-    commandindex.init(
-      p_getProcAddress,
-      p_library)
-
-    r_initialization.initialize = initialize
-    r_initialization.deinitialize = deinitialize
-
-    return true
-
-  except:
-    return false
-
-
 template GDExtension_EntryPoint*(name): untyped =
-  bind init
-  bind gdcall
-  proc name*(p_get_proc_address: InterfaceGetProcAddress; p_library: ClassLibraryPtr; r_initialization: ptr Initialization): Bool {.gdcall, exportc, dynlib.} =
-    init(p_getProcAddress, p_library, r_initialization)
+  {.emit: "N_LIB_EXPORT N_CDECL(void, NimMain)(void);".}
+  proc name*(p_get_proc_address: InterfaceGetProcAddress; p_library: ClassLibraryPtr; r_initialization: ptr Initialization): Bool {.gdcall, exportc, dynlib.} = once:
+    try:
+      commandindex.init(
+        p_getProcAddress,
+        p_library)
+
+      r_initialization.initialize = initialize
+      r_initialization.deinitialize = deinitialize
+
+      commandindex.load() # call `getProcAddr` for each interface methods
+      builtinindex.load() # load builtin-classes constructor/destructor for ORC hooks
+      extracommands.load() # load other usual functions to define core library
+      typeshift.load()
+      utilityfuncs.load()
+
+      invoke staticevents.init_engine.on_load_builtinclassConstructor # expects to register by generateds.
+      invoke staticevents.init_engine.on_load_builtinclassOperator
+      invoke staticevents.init_engine.on_load_builtinclassMethod
+      {.emit: "NimMain();".}
+
+      return true
+
+    except:
+      return false
 
 when isMainModule:
   GDExtension_EntryPoint(init_library)
