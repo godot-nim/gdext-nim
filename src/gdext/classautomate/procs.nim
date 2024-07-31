90972866618d0ab13551b25a33d02bda4c66597b
la .panon. <panno.800k@gmail.com>
2024-07-31 12:53:05 +0900

    Support to define and export global procs

--- src/gdext/classautomate/procs.nim
+++ src/gdext/classautomate/procs.nim
@@ -1,14 +1,15 @@
 import std/macros
+import std/sequtils
 
 import contracts
 import methodinfo
+import propertyinfo
 import checkform
 
 import gdextcore/commandindex
 import gdextcore/gdclass
 
-proc sync_procDef*(procdef: NimNode): NimNode =
-  precheckIsCorrectClassMethod: procdef
+macro registerProc(procdef): untyped =
   let name = procdef.name
   let arg0_T = procdef.params[1][1]
 
@@ -24,8 +25,49 @@ proc sync_procDef*(procdef: NimNode): NimNode =
 
   let methodinfoDef = procdef.classMethodInfo(gdname)
 
-  procdef.withCorrectClassMethodForm quote do:
-    `procdef`
+  quote do:
     process(contract(`arg0T`).procedure, `gdname`):
       let glue = `methodinfoDef`
       interface_ClassDB_registerExtensionClassMethod(environment.library, addr className(typedesc `arg0_T`), addr glue.info)
+
+proc makeNimMainProc(procdef: NimNode): NimNode =
+  newProc(
+    name = ident $procdef.name,
+    params = concat(
+      @[procdef.params[0]],
+      @[newIdentDefs(ident"_", bindsym"typeof".newcall(ident"extmain"))],
+      procdef.params[1..^1]),
+    pragmas = procdef.pragma,
+    body = procdef.name.newCall(procdef.params[1..^1].mapIt(it[0])),
+    procType = procdef.kind,
+  )
+
+proc sync_procDef*(procdef: NimNode): NimNode =
+  let arg0T = procdef.params[1][1]
+
+  result = newNimNode nnkWhenStmt
+
+  for i, arg in procdef.params:
+    if i == 0: continue
+    let argT = arg[1]
+    result.add nnkElifBranch.newTree(
+      (quote do: `argT` isnot SomeProperty),
+      bindsym"lineerror".newcall(newlit "invalid form; the type `" & argT.repr & "` is not supported for argument.", arg)
+    )
+
+  result.add nnkElifBranch.newTree(
+    quote do:
+      `arg0T` is GodotClass,
+    quote do:
+      `procdef`
+      registerProc `procdef`
+  )
+
+  let procNimMain = makeNimMainProc procdef
+
+  result.add nnkElse.newTree(
+    quote do:
+      `procdef`
+      `procNimMain`
+      registerProc `procNimMain`
+  )
