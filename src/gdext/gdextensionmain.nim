f23778fee201674b886bc4558e520a1d882a49f9
la .panon. <panno.800k@gmail.com>
2024-07-31 12:50:47 +0900

    Add ExtensionMain class that express nim's top-level codes

---
+++ src/gdext/gdextensionmain.nim
@@ -0,0 +1,33 @@
+import gdextcore/dirty/gdextensioninterface
+import gdextcore/[gdclass]
+import gdextgen/[classindex, builtinclasses]
+import gdextgen/classes/[gdengine]
+import gdext/[env, init, classautomate, classtraits]
+import gdext/classautomate/contracts
+
+import std/macros
+
+macro ExtensionMain*: untyped = ident ExtensionMainName
+
+macro defExtensionMain: untyped =
+  let typ = ident ExtensionMainName
+  quote do:
+    type `typ`* = ref object of Object
+
+defExtensionMain
+
+var extmain*: ExtensionMain
+
+method init(self: ExtensionMain) =
+  discard
+
+proc setupExtensionMain =
+  extmain = instantiate ExtensionMain
+  Engine.singleton.registerSingleton(className ExtensionMain, extmain)
+
+process initialize_before_scene, "extmain":
+  register ExtensionMain
+  setupExtensionMain()
+
+process eliminate_scene, "extmain":
+  extmain = nil
