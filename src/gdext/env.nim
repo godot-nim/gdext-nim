62e88be521de9009dc3df937d4d069ae7a609b73
la .panon. <panno.800k@gmail.com>
2024-08-04 16:22:34 +0900

    Get projectName internally

--- src/gdext/env.nim
+++ src/gdext/env.nim
@@ -4,6 +4,7 @@ This project must be compiled as a dynamic library.
 Instead, run `nimble build --app:lib` or append `--app: lib` to the config.nims file.
 """.}
 
-import std/strutils
-const projectName {.strdefine.} = "anonymous"
-const ExtensionMainName* = projectName.capitalizeAscii
+import std/[strutils, os, macros]
+const workspacePath* = getProjectPath()
+const workspaceName* = workspacePath.splitFile.name
+const ExtensionMainName* {.strdefine.} = workspaceName.capitalizeAscii
