ea73222ce311eb87665c66e7a5883e237ece89da
la .panon. <panno.800k@gmail.com>
2024-08-05 09:50:07 +0900

    Remove workspace system-dependent processing

--- src/gdext/env.nim
+++ src/gdext/env.nim
@@ -1,10 +1,12 @@
+const ExtensionMainName* {.strdefine: "ExtensionMain".} = ""
+
 when appType != "lib":
   {.error: """
 This project must be compiled as a dynamic library.
 Instead, run `nimble build --app:lib` or append `--app: lib` to the config.nims file.
 """.}
 
-import std/[strutils, os, macros]
-const workspacePath* = getProjectPath()
-const workspaceName* = workspacePath.splitFile.name
-const ExtensionMainName* {.strdefine.} = workspaceName.capitalizeAscii
+when ExtensionMainName == "":
+  {.error: """
+prease set `--define:ExtensionMain:YourUniqueExtensionName`.
+""".}
