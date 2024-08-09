1de5f310cb904705d0e661ec218cf9936b35139e
la .panon. <panno.800k@gmail.com>
2024-08-09 16:04:30 +0900

    remove GdRef->RefCounted converter
    This is confusing because it is not possible to call methods of the parent class.

--- src/gdext/conversions.nim
+++ src/gdext/conversions.nim
@@ -1,9 +1,7 @@
 import gdextcore/builtinindex
 import gdextgen/builtinclasses/constructors
-import gdextcore/gdrefs
 
 
 converter convertToString*(str: string): String = gdstring str
 converter convertToStringName*(str: string): StringName = stringName str
 converter convertToNodePath*(str: string): NodePath = nodePath gdstring str
-converter convertToRefCounted*[T: SomeRefCounted](x: GdRef[T]): T = x.unwrapped
