d2ba93997a207f6c58e964c332a6326116e9e467
la .panon. <panno.800k@gmail.com>
2024-08-19 19:02:23 +0900

    remove `import extentionmanager`

--- src/gdext.nim
+++ src/gdext.nim
@@ -10,9 +10,6 @@ export            builtinclasses, classindex, globalenums, localenums, structs
 import gdext/[ env, init, classautomate, classtraits, varianttraits, nodetraits, gdrefs, conversions ]
 export         env, init, classautomate, classtraits, varianttraits, nodetraits, gdrefs, conversions
 
-import gdext/extensionmanager
-export extensionmanager.isRunningInEditor
-
 import gdext/gdextensionmain
 export gdextensionmain.ExtensionMain, gdextensionmain.extmain
 
