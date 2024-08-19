929835c3f7176207895745f08c9d49965fb94fe7
la .panon. <panno.800k@gmail.com>
2024-08-19 19:07:22 +0900

    export `isEditorHint`

--- src/gdext.nim
+++ src/gdext.nim
@@ -16,6 +16,9 @@ export gdextensionmain.ExtensionMain, gdextensionmain.extmain
 import gdextgen/utilityfuncs
 export utilityfuncs except print
 
+import gdextgen/classes/gdengine
+export gdengine.isEditorHint
+
 proc print*(args: varargs[Variant, variant]) =
   if unlikely(args.len == 0): return
   utilityfuncs.print(args[0], args[1..^1])
