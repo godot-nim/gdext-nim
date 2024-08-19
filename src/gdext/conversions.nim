4473265066abd585b56abd220ad0a8f798093d86
la .panon. <panno.800k@gmail.com>
2024-08-19 16:43:32 +0900

    add toSingleton converter

--- src/gdext/conversions.nim
+++ src/gdext/conversions.nim
@@ -1,7 +1,10 @@
 import gdextcore/builtinindex
+import gdextcore/gdclass
 import gdextgen/builtinclasses/constructors
-
+import gdext/classtraits
 
 converter convertToString*(str: string): String = gdstring str
 converter convertToStringName*(str: string): StringName = stringName str
 converter convertToNodePath*(str: string): NodePath = nodePath gdstring str
+
+converter toSingleton*[T: GodotClass](_: typedesc[T]): T = singleton(T)
