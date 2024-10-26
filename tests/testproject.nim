9767686d827d90a8296973b50c5c698a627503dd
la .panon. <panno.800k@gmail.com>
2024-10-26 15:10:08 +0900

    Test: simplify testproject.nim

--- tests/testproject.nim
+++ tests/testproject.nim
@@ -1,11 +1,3 @@
 import std/[os]
 
-proc pipe(result: 0..1) =
-  if result == 1: quit 1
-
-pipe execShellCmd"nim c testproject/runtime/nim/bootstrap"
-
-if dirExists"testproject/runtime/.godot":
-  pipe execShellCmd"godot --path testproject/runtime"
-else:
-  pipe execShellCmd"godot --path testproject/runtime --editor"
+quit execShellCmd"gdextwiz run testproject/runtime"
