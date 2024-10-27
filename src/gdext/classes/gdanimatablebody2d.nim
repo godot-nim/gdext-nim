b819c3e9786a154be74b538539e0f3f24a1bcefd
othd06 <othdowning@outlook.com>
2024-10-27 18:17:10 +0000

    added documentation comments to the gdAnimatableBody2D class

--- src/gdext/classes/gdanimatablebody2d.nim
+++ src/gdext/classes/gdanimatablebody2d.nim
@@ -5,11 +5,17 @@ import gdext/coronation/header/classes
 import gdstaticbody2d; export gdstaticbody2d
 
 proc setSyncToPhysics*(self: AnimatableBody2D; enable: bool): void =
+  ##If true, the body's movement will be synchronized to the physics frame. This is useful when animating
+  ##movement via AnimationPlayer, for example on moving platforms. Do not use together with
+  ##PhysicsBody2D.move_and_collide.
   expandMethodBind(className AnimatableBody2D, "set_sync_to_physics", 2586408642)
   var `?param` = [getPtr enable]
   methodbind.ptrcall(self, addr `?param`[0])
 
 proc isSyncToPhysicsEnabled*(self: AnimatableBody2D): bool =
+  ##If true, the body's movement will be synchronized to the physics frame. This is useful when animating
+  ##movement via AnimationPlayer, for example on moving platforms. Do not use together with
+  ##PhysicsBody2D.move_and_collide.
   expandMethodBind(className AnimatableBody2D, "is_sync_to_physics_enabled", 36873697)
   var ret: encoded bool
   methodbind.ptrcall(self, nil, addr ret)
