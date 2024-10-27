ba3e69a1b2680d230d3f5b34debdf13eb430df8d
othd06 <othdowning@outlook.com>
2024-10-27 18:14:02 +0000

    added documentation comments to the gdAesContext class

--- src/gdext/classes/gdaescontext.nim
+++ src/gdext/classes/gdaescontext.nim
@@ -5,6 +5,9 @@ import gdext/coronation/header/classes
 import gdrefcounted; export gdrefcounted
 
 proc start*(self: AesContext; mode: AesContext_Mode; key: PackedByteArray; iv: PackedByteArray = PackedByteArray()): Error =
+  ##Start the AES context in the given mode. A key of either 16 or 32 bytes must always be provided, while an
+  ##iv (initialization vector) of exactly 16 bytes, is only needed when mode is either MODE_CBC_ENCRYPT or
+  ##MODE_CBC_DECRYPT.
   expandMethodBind(className AesContext, "start", 3122411423)
   var `?param` = [getPtr mode, getPtr key, getPtr iv]
   var ret: encoded Error
@@ -12,6 +15,10 @@ proc start*(self: AesContext; mode: AesContext_Mode; key: PackedByteArray; iv: P
   (addr ret).decode_result(Error)
 
 proc update*(self: AesContext; src: PackedByteArray): PackedByteArray =
+  ##Run the desired operation for this AES context. Will return a PackedByteArray containing the result of
+  ##encrypting (or decrypting) the given src. See start for mode of operation.
+  ##
+  ##Note: The size of src must be a multiple of 16. Apply some padding if needed.
   expandMethodBind(className AesContext, "update", 527836100)
   var `?param` = [getPtr src]
   var ret: encoded PackedByteArray
@@ -19,12 +26,17 @@ proc update*(self: AesContext; src: PackedByteArray): PackedByteArray =
   (addr ret).decode_result(PackedByteArray)
 
 proc getIvState*(self: AesContext): PackedByteArray =
+  ##Get the current IV state for this context (IV gets updated when calling update). You normally don't need this
+  ##function.
+  ##
+  ##Note: This function only makes sense when the context is started with MODE_CBC_ENCRYPT or MODE_CBC_DECRYPT.
   expandMethodBind(className AesContext, "get_iv_state", 2115431945)
   var ret: encoded PackedByteArray
   methodbind.ptrcall(self, nil, addr ret)
   (addr ret).decode_result(PackedByteArray)
 
 proc finish*(self: AesContext): void =
+  ##Close this AES context so it can be started again. See start.
   expandMethodBind(className AesContext, "finish", 3218959716)
   methodbind.ptrcall(self, nil)
 
