d39fc43ba33bee42395d0debe3486f73abce826a
la .panon. <panno.800k@gmail.com>
2024-11-21 20:42:10 +0900

    Fix: specify enum of index to avoid type mismatch

--- coronation/src/coronation/operators/classes/properties.nim
+++ coronation/src/coronation/operators/classes/properties.nim
@@ -3,6 +3,7 @@ import ../classindex
 
 import submodules/wordropes
 import submodules/semanticstrings
+import operators/arguments
 
 import std/options
 import std/strformat
@@ -10,9 +11,16 @@ import std/strformat
 proc weave_properties*(class: Class): Cloth =
   weave margin:
     for prop in class.json.properties.get(@[]):
+      var typ =
+        if prop.index.isSome:
+          var metho = class.methods[prop.getter]
+          metho.arguments.get(@[])[0].convert().typeSym
+        else:
+          TypeSym.Void
+
       weave multiline:
-        let index_get = (if prop.index.isSome: $prop.index.get else: "")
-        let index_set = (if prop.index.isSome: $prop.index.get & ", " else: "")
+        let index_get = (if prop.index.isSome: &"{typ}({prop.index.get})" else: "")
+        let index_set = (if prop.index.isSome: index_get & ", " else: "")
         let name = prop.name.scan.convert(ProcSym)
         &"template {name}*(self: {class.typesym}): untyped = self.{prop.getter.scan.convert(ProcSym)}({index_get})"
         if prop.setter.isSome:
