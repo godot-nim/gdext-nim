b099a75c9595c7e61dc52f0d12f4e1286fb5a4af
la .panon. <panno.devel@gmail.com>
2025-02-22 16:12:58 +0900

    Feat(wizard): update template

--- src/gdext/wizard/subcommands/extension/template/bootstrap.nim
+++ src/gdext/wizard/subcommands/extension/template/bootstrap.nim
@@ -1,16 +1,5 @@
 import gdext
-
-# import your extension classes here
-# import myclass
-
-# ==================================
-
-proc register_classes {.execon: initialize_scene.} =
-  # register your extension classes here
-  # register MyClass
-
-  # ====================================
-  discard
+import classes/gdMyClass
 
 
 GDExtensionEntryPoint
