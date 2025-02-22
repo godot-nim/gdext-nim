1a74cfe70d1841721c779a35819aa91041e2443c
la .panon. <panno.devel@gmail.com>
2025-02-23 01:28:21 +0900

    Refactor: improve signal/proc-generation's readability

---
+++ src/gdext/core/userclass/tools.nim
@@ -0,0 +1,33 @@
+import std/[sequtils]
+
+import gdext/utils/macros
+import gdext/core/gdclass
+import propertyinfo
+
+proc withCheckTypes*(node, onSelfTypeFailed, onDefault: NimNode): NimNode =
+  let lineerror = bindSym "lineerror"
+  result = newNimNode nnkWhenStmt
+
+  if node.hasReturn:
+    let arg = node.params[0]
+    result.add newElifBranch( (quote do: `arg` isnot SomeProperty),
+      lineerror.newcall(newlit "invalid form; the type `" & arg.repr & "` is not supported for result.", arg)
+    )
+
+  let args = node.params.breakArgs.toSeq
+  for (i, arg) in args:
+    var argT = arg.Type or bindSym"typeof".newCall arg.default
+    if argT.isVarargs:
+      if i != args.high:
+        error "invalid form; varargs must be placed at last.", arg.name
+      else:
+        argT = argT[1]
+    result.add newElifBranch( (quote do: `argT` isnot SomeProperty),
+      lineerror.newcall(newlit "invalid form; the type `" & argT.repr & "` is not supported for argument.", arg.name)
+    )
+
+  let Self = node.params[1][1]
+  result.add newElifBranch( (quote do: `Self` isnot SomeUserClass),
+    onSelfTypeFailed)
+
+  result.add newElse(onDefault)
