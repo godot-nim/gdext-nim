fd37951ba3d5f49c0347be065adcc0d3b2930de5
la .panon. <panno.devel@gmail.com>
2025-02-27 19:13:09 +0900

    Feat: set[enum] for properties

--- testproject/runtime/nim/src/classes/gdextnode/enums.nim
+++ testproject/runtime/nim/src/classes/gdextnode/enums.nim
@@ -14,12 +14,21 @@ type TestEnumB = enum
   EnumB2
   EnumB3 = (4, "ENUM!")
 
+type TestFlags = enum
+  Flag1
+  Flag2
+  Flag3
+  Flag4
+  Flag5
+
 
 GDExtNode.registerEnum TestEnumA
 GDExtNode.registerEnum TestEnumB
+GDExtNode.registerBitField TestFlags
 
 var testEnumA: TestEnumA = EnumA2
 var testEnumB: TestEnumB = EnumB2
+var testFlags: set[TestFlags] = {Flag2}
 
 gdexport "test_enum_a",
   proc(x: GDExtNode): TestEnumA = testEnumA,
@@ -29,6 +38,11 @@ gdexport "test_enum_b",
   proc(x: GDExtNode): TestEnumB = testEnumB,
   proc(x: GDExtNode; value: TestEnumB) = testEnumB = value
 
+gdexport "test_flags",
+  proc(x: GDExtNode): set[TestFlags] = testFlags,
+  proc(x: GDExtNode; value: set[TestFlags]) = testFlags = value
+
 proc echoTestEnumA(self: GDextNode; x: TestEnumA): TestEnumA {.gdsync.} = x
 proc echoTestEnumB(self: GDextNode; x: TestEnumB): TestEnumB {.gdsync.} = x
+proc echoTestFlags(self: GDextNode; x: set[TestFlags]): set[TestFlags] {.gdsync.} = x
 proc echoVector2Axis(self: GDextNode; x: Vector2_Axis): Vector2_Axis {.gdsync.} = x
