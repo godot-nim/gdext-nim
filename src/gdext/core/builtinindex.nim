93d2daf943be43f63972348b440ac5f40bb38609
la .panon. <panno.devel@gmail.com>
2025-03-06 17:27:27 +0900

    Fix: Add missing `wasMoved` to `=copy(Variant)`

--- src/gdext/core/builtinindex.nim
+++ src/gdext/core/builtinindex.nim
@@ -337,6 +337,7 @@ proc `=dup`*(x: Variant): Variant =
   interface_variantNewCopy(addr result, addr x)
 proc `=copy`*(dest: var Variant; source: Variant) =
   `=destroy` dest
+  wasMoved dest
   interface_variantNewCopy(addr dest, addr source)
 
 method onInit*(self: Object) {.base.} = discard
