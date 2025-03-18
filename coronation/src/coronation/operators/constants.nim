982329d89b3e2fa1559193b8672d7758bfbe8503
la .panon. <panno.devel@gmail.com>
2025-03-19 02:57:47 +0900

    [Req-Generate] Rename constants

--- coronation/src/coronation/operators/constants.nim
+++ coronation/src/coronation/operators/constants.nim
@@ -1,6 +1,6 @@
 import cloths
 
-import submodules/semanticstrings
+import submodules/[wordropes, semanticstrings]
 import types/json
 import utils
 
@@ -17,7 +17,7 @@ proc constValue*(t: string; value: string): string =
     value.replace(t, $constructorName TypeSym t)
 
 proc weave*(constant: JsonConstant; caller: TypeSym): Cloth =
-  let pred = constant.name.convert(TypeSym)
+  let pred = constant.name.scan().convert(TypeSym)
   let value = constValue(constant.`type`, constant.value)
   if caller in [TypeSym"Color"]:
     weave multiline:
