958977f890e5471b7c40efb923f539359cc006c8
la .panon. <panno.devel@gmail.com>
2025-09-09 22:09:45 +0900

    add versions module

---
+++ src/gdext/versions.nim
@@ -0,0 +1,18 @@
+import std/[strutils, sequtils]
+
+proc strToTuple(str: string): tuple[Major, Minor, Patch: int] =
+  let s = str.split(".").map(parseInt)
+  (s[0], s[1], s[2])
+
+const
+  GdextVersionString* = "0.14.0"
+  GdextVersion* = strToTuple(GdextVersionString)
+  GdextVersionMajor* = GdextVersion.Major
+  GdextVersionMinor* = GdextVersion.Minor
+  GdextVersionPatch* = GdextVersion.Patch
+
+  CurrentSupportedGodotVersionString* = "4.4.1"
+  CurrentSupportedGodotVersion* = strToTuple(CurrentSupportedGodotVersionString)
+  CurrentSupportedGodotVersionMajor* = CurrentSupportedGodotVersion.Major
+  CurrentSupportedGodotVersionMinor* = CurrentSupportedGodotVersion.Minor
+  CurrentSupportedGodotVersionPatch* = CurrentSupportedGodotVersion.Patch
