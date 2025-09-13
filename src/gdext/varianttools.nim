6d1ecc09bcbd58b519bd4e03179649d6290aab13
la.panon. <panno.devel@gmail.com>
2025-09-13 15:03:15 +0900

    fix: `variant(newTypedArray[Int]()) of TypedArray[Float]` should be false (#294)

--- src/gdext/varianttools.nim
+++ src/gdext/varianttools.nim
@@ -1,6 +1,7 @@
 import std/[strformat, hashes, sequtils]
 
 import gdext/builtinindex
+import gdext/arraytools
 import gdext/private/[gdinterface, typeshift]
 
 proc iterInit(self: Variant; r_iter: var Variant; r_valid: var bool): bool =
@@ -253,10 +254,14 @@ iterator pairs*(self: Variant): tuple[key, item: Variant] =
   for key in self.keys: yield (key, self[key])
 
 proc `of`*[T: SomeProperty](a: Variant; b: typedesc[T]): bool =
+  when T is TypedArray:
+    var empty {.global.} = newTypedArray[T.T]()
   {.hint[CondTrue]: off.}
   result = if a.getType == b.variantType:
     when b is Object:
       (a as Object) of b
+    elif b is TypedArray:
+      (a as Array).isSameTyped(empty)
     else:
       true
   else:
