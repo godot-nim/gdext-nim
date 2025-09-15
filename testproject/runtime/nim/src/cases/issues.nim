23e24b4f92de311c2a824638f0128bf341859841
the-key-point <10120823+the-key-point@users.noreply.github.com>
2025-09-16 00:52:56 +0300

    Set method automatic variant conversion sugar (#301)
    * achieved by la.panon.'s implementation, adding sugar to set variant
    conversion a tiny QoL
    
    * Fixed specific library import and appropriate type name convention,
    added line to destroy the node after checking.

--- testproject/runtime/nim/src/cases/issues.nim
+++ testproject/runtime/nim/src/cases/issues.nim
@@ -1,6 +1,9 @@
 import gdext
-import gdext/classes/[gdCSGSphere3D, gdStandardMaterial3D]
+import gdext/classes/[gdCSGSphere3D, gdStandardMaterial3D,gdNode]
 import testutils
+type
+  TestNode* {.gdsync.} = ptr object of Node
+    a* {.gdexport.}: int
 
 runtime: suite "Community Reported":
 
@@ -25,3 +28,10 @@ runtime: suite "Community Reported":
     let myString : String = "test"
     myPackedString.add(myString)
     check myPackedString[0] == myString
+
+  test "object set should compile and work":
+    var myTestNode = TestNode.instantiate
+    myTestNode.set("a",1)
+    let a = myTestNode.get("a").as(int)
+    check a == 1
+    destroy myTestNode
