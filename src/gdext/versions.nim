94dea8528c8b8a9aae90168f968eca2c51b96eec
la .panon. <panno.devel@gmail.com>
2025-09-18 15:29:59 +0900

    update supported godot version to 4.5

--- src/gdext/versions.nim
+++ src/gdext/versions.nim
@@ -11,7 +11,7 @@ const
   GdextVersionMinor* = GdextVersion.Minor
   GdextVersionPatch* = GdextVersion.Patch
 
-  CurrentSupportedGodotVersionString* = "4.4.1"
+  CurrentSupportedGodotVersionString* = "4.5.0"
   CurrentSupportedGodotVersion* = strToTuple(CurrentSupportedGodotVersionString)
   CurrentSupportedGodotVersionMajor* = CurrentSupportedGodotVersion.Major
   CurrentSupportedGodotVersionMinor* = CurrentSupportedGodotVersion.Minor
