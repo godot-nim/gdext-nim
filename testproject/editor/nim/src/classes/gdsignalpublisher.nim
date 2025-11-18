b434159984a746d91d318f5fd04f2aea7c94e8e7
la.panon. <panno.devel@gmail.com>
2025-11-18 11:45:10 +0900

    Support nim 2.2.6 (#321)
    * Avoiding Regression in Nim 2.2.6
    
    * update test case
    
    * test: using Nim 2.2.6

--- testproject/editor/nim/src/classes/gdsignalpublisher.nim
+++ testproject/editor/nim/src/classes/gdsignalpublisher.nim
@@ -4,10 +4,14 @@ randomize()
 
 type SignalPublisher* {.tool.} = ptr object of Node
   key: Int
+  seconds: float = 1
 
 proc send*(self: SignalPublisher; key: Int): Error {.signal, gdsync.}
 
 method process(self: SignalPublisher; delta: float64) {.gdsync.} =
   if self.key == 0:
     self.key = randiRange(0, 255)
-  assert self.send(self.key) == ok
+  self.seconds += delta
+  if self.seconds >= 1.0:
+    assert self.send(self.key) == ok
+    self.seconds = 0
