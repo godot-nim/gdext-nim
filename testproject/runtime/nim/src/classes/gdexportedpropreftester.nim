43681d27d2592d8fac7508f0103f7a57fad127a3
rrenode <robert.renodej@gmail.com>
2026-04-08 14:04:01 -0400

    reanmed refcountertester module

--- testproject/runtime/nim/src/classes/gdexportedpropreftester.nim
+++ testproject/runtime/nim/src/classes/gdexportedpropreftester.nim
@@ -3,9 +3,9 @@ import gdext/classes/[
   gdNode, gdTexture2D, gdResourceLoader
   ]
 
-type MyBuggedNodeBad* {.gdsync.} = ptr object of Node
+type ExportedPropRefTester* {.gdsync.} = ptr object of Node
   clsProp* {.gdexport.}: gdref Texture2D
 
-method ready(self: MyBuggedNodeBad) {.gdsync.} =
+method ready(self: ExportedPropRefTester) {.gdsync.} =
   var resource: gdref Resource = ResourceLoader.load("res://icon.svg")
   self.clsProp = resource as gdref Texture2D
